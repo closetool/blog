8b72d9b40c2e27b6610305ac1d2a8e2b3ecdd538
closetool <c299999999@qq.com>
2020-10-22 22:54:40 +0800

    修正了错误

--- utils/collectionsutils/collectionutils_test.go
+++ utils/collectionsutils/collectionutils_test.go
@@ -1,32 +1,15 @@
 package collectionsutils
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/closetool/blog/system/initial"
-	"github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
-	"github.com/spf13/viper"
 )
-
-func init() {
-	initial.InitConfig("collectionutils")
-	viper.Set("log_level", fmt.Sprintf("%d", logrus.DebugLevel))
-	initial.InitLog()
-	//config.LoadConfigurationFromBranch(
-	//	viper.GetString("config_server_url"),
-	//	"userservice",
-	//	viper.GetString("profile"),
-	//	viper.GetString("branch"),
-	//)
-}
 
 func TestRandomString(t *testing.T) {
 	Convey("Random String", t, func() {
 		res := RandomString(32)
 		Convey("result should have 32 bit", func() {
-			logrus.Debugf("res = %v\n", string(res))
 			So(res, ShouldHaveLength, 32)
 		})
 	})
